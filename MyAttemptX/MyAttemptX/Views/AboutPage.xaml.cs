2f762466000f44c588c859e0f4d242dad2749527
AURELIUS\mcsim <max.yermachonak@gmail.com>
2017-05-17 13:29:15 +0300

    Add project files.

---
+++ MyAttemptX/MyAttemptX/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace MyAttemptX.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
